125f6595f412733e0a0034fbd3c68eb40c9194a8
JoshhuaP <Joshua.pinneau@gmail.com>
2021-11-29 15:33:52 +0100

    Part 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,11 +3,33 @@ import java.util.Scanner;
 public class Launcher {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Bienvenue, tapez 'quit' pour quitter le programme");
+        System.out.println("Bienvenue, \ntapez 'quit' pour quitter le programme \nTapez 'fibo' pour découvrir la suite de Fibonacci");
         String quit = scanner.nextLine();
         while (!quit.equals("quit")) {
-            System.out.println("Unknown command");
-            quit = scanner.nextLine();
+            if (quit.equals("fibo")) {
+                System.out.println("Entrez une valeur de n");
+                int n = scanner.nextInt();
+                int Fibo=0;
+                int F0=0;
+                int F1=1;
+                int F2;
+                int i=2;
+                if (n == 0) Fibo = F0;
+                if (n == 1) Fibo = F1;
+                while(i < n) {
+                    F2 = F0+F1;
+                    F0 = F1;
+                    F1 = F2;
+                    Fibo = F2;
+                    i++;
+                }
+                System.out.println("Le résultat correspondant est le suivant : "+Fibo);
+                scanner.nextLine();
+            }
+            else {
+                System.out.println("Unknown command");
+            }
+                quit = scanner.nextLine();
         }
     }
 }
