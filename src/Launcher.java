bef6e0521445c6eceead231662cb37a7b16abb5c
JoshhuaP <Joshua.pinneau@gmail.com>
2021-11-29 14:52:58 +0100

    Part 3

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,8 +5,9 @@ public class Launcher {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bienvenue, tapez 'quit' pour quitter le programme");
         String quit = scanner.nextLine();
-        if (!quit.equals("quit")) {
+        while (!quit.equals("quit")) {
             System.out.println("Unknown command");
+            quit = scanner.nextLine();
         }
     }
 }
