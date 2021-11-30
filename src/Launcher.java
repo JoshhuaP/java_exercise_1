69c23cb0de6a15e1d0aa7761523c89e532c3faf4
JoshhuaP <Joshua.pinneau@gmail.com>
2021-11-30 10:55:55 +0100

    Part 5

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,9 +1,17 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 
 public class Launcher {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Bienvenue, \ntapez 'quit' pour quitter le programme \nTapez 'fibo' pour découvrir la suite de Fibonacci");
+        System.out.println("Bienvenue, \ntapez 'quit' pour quitter le programme \nTapez 'fibo' pour découvrir la suite de Fibonacci\nTapez 'freq' pour découvrir les trois mots les plus fréquents dans un fichier texte");
         String quit = scanner.nextLine();
         while (!quit.equals("quit")) {
             if (quit.equals("fibo")) {
@@ -24,6 +32,36 @@ public class Launcher {
                     i++;
                 }
                 System.out.println("Le résultat correspondant est le suivant : "+Fibo);
+                scanner.nextLine();
+            }
+            if (quit.equals("freq")) {
+                System.out.println("Entrez un chemin de fichier");
+                String file = scanner.nextLine();
+                Path PathFile = Paths.get(file);
+                String word=null;
+                ArrayList<String> wordsTab = new ArrayList<String>();
+                try {
+                    word = Files.readString(PathFile);
+                }
+                catch (IOException e) {
+                    System.out.println("Unreadable file: IOException ");
+                    e.printStackTrace();
+                    return;
+                }
+                String [] words = word.split(" ");
+                for (int i=0; i<words.length;i++) {
+                    if (words[i].isBlank()) {
+                        continue;
+                    }
+                    words[i].toLowerCase();
+                    wordsTab.add(words[i]);
+                }
+                Map<String, Long> wordsCount = wordsTab.stream().collect(Collectors.groupingBy(x->x, Collectors.counting()));
+                List<Map.Entry<String, Long>> list = new ArrayList<>(wordsCount.entrySet());
+                list.sort(Comparator.comparingLong(Map.Entry::getValue));
+                for (int i=Math.max(list.size()-3, 0); i<list.size();i++) {
+                    System.out.println(list.get(i));
+                }
                 scanner.nextLine();
             }
             else {
